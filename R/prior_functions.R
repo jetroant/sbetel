2596e756fb614e2e7ff51034104ff977eaa3e136
jetroant <jetro.a@gmail.com>
2020-05-20 00:16:53 +0300

    Correct prior scheme

--- R/prior_functions.R
+++ R/prior_functions.R
@@ -1,6 +1,10 @@
 
 #Prior generating function for var without constant
-prior_fun_var <- function(model, nn = 50, epsilon = 0.1, sigma_x = 0.1) {
+prior_fun_var <- function(model, 
+                          nn = floor(nrow(model$args$xy$yy)/model$args$p), 
+                          epsilon = model$args$epsilon_0) {
+  
+  if(is.null(model$args$epsilon_0)) epsilon <- 0.1
   
   p <- model$args$p
   m <- ncol(model$y)
@@ -9,11 +13,13 @@ prior_fun_var <- function(model, nn = 50, epsilon = 0.1, sigma_x = 0.1) {
   prior_mean <- matrix(0, nrow = m*p, ncol = m)
   prior_mean[c(1:m),] <- diag(model$args$stat)
   
+  xx <- model$args$xy$xx
   xx0 <- matrix(0, nrow = nn*p, ncol = m*p)
   for(i in 1:p) {
     rows <- (i*nn+1):(i*nn+nn)-nn
     cols <- (i*m+1):(i*m+m)-m
-    xx0[rows,cols] <- rnorm(nn*m, 0, sigma_x*i)
+    rows_xx <- sample.int(nrow(xx), nn, replace = TRUE)
+    xx0[rows,cols] <- xx[rows_xx, cols + 1]*i
   }
   
   yy0 <- xx0 %*% prior_mean
