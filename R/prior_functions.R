1db45fef31e1fdda0a5f645fc6c61de4822bab6d
jetroant <jetro.a@gmail.com>
2020-05-20 20:32:00 +0300

    Add ar-epsilon scheme to prior_fun_var()

--- R/prior_functions.R
+++ R/prior_functions.R
@@ -1,32 +1,36 @@
 
 #Prior generating function for var without constant
-prior_fun_var <- function(model, 
-                          nn = floor(nrow(model$args$xy$yy)/model$args$p), 
-                          epsilon = model$args$epsilon_0) {
+prior_fun_var <- function(model) {
   
-  if(is.null(model$args$epsilon_0)) epsilon <- 0.1
-  
+  nn = floor(nrow(model$args$xy$yy)/model$args$p)
+  xy <- model$args$xy
   p <- model$args$p
   m <- ncol(model$y)
   stat <- model$args$stat
+  if(is.null(model$args$lambda)) lambda <- 1 else lambda <- model$args$lambda
+  
+  epsilon <- rep(NA, m)
+  for(i in 1:m) epsilon[i] <- sqrt(ar(xy$yy[,i], aic = F, order.max = 1)$var.pred)
   
   prior_mean <- matrix(0, nrow = m*p, ncol = m)
   prior_mean[c(1:m),] <- diag(model$args$stat)
   
-  xx <- model$args$xy$xx
+  xx <- xy$xx
   xx0 <- matrix(0, nrow = nn*p, ncol = m*p)
   for(i in 1:p) {
     rows <- (i*nn+1):(i*nn+nn)-nn
     cols <- (i*m+1):(i*m+m)-m
     rows_xx <- sample.int(nrow(xx), nn, replace = TRUE)
-    xx0[rows,cols] <- xx[rows_xx, cols + 1]*i
+    xx0[rows,cols] <- xx[rows_xx, cols + 1]*i*lambda
   }
   
   yy0 <- xx0 %*% prior_mean
   for(i in 1:p) {
     rows <- (i*nn+1):(i*nn+nn)-nn
     cols <- (i*m+1):(i*m+m)-m
-    yy0[rows,] <- yy0[rows,] + rnorm(nn*m, 0, epsilon)
+    for(j in 1:m) {
+      yy0[rows,j] <- yy0[rows,j] + rnorm(nn, 0, epsilon[j])
+    }
   }
   
   list("yy" = yy0, "xx" = xx0)
